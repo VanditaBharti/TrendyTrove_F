8975fa47ab0dece294d4bef8c17a04c88459413c
Vandita Bharti <kajalk0306@gmail.com>
2024-07-12 00:12:39 +0530

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useRef, useState} from "react";
 import "./Navbar.css";
 import tt_logo from "../Assets/tt_logo.jpeg";
 import GB_cart from "../Assets/GB_cart.jpg";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
 
+import nav_dropdown from '../Assets/nav_dropdown_icon.png'
+
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const {getTotalCartItems}= useContext(ShopContext);
+  const menuRef =useRef();
+
+  const dropdown_toggle=(e)=>{
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className="navbar">
@@ -15,8 +23,10 @@ const Navbar = () => {
         <img src={tt_logo} alt="" />
         <p>TrendyTrove</p>
       </div>
+
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       
-        <ul className="nav-menu">
+        <ul ref={menuRef} className="nav-menu">
           <li
           onClick = {()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/> : <></> }
           </li>
